d04a29c804c3b62f319d4d3a8fbbcee1d4944364
Geetanjali Patil <patilgeetanjali2001@gmail.com>
2024-09-15 16:48:33 +0530

    Assignment Completed

--- Assignment_9/q5.c
+++ Assignment_9/q5.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
-typedef struct student{
+typedef struct Student{
 
-	int rollno;
 	char name[20];
+	int ID;
 	float marks;
 }stud_t;
 
@@ -12,12 +12,34 @@ void readData(stud_t, *s);
 
 int main()
 {
-	
+	Student student1 = {"abc", 1, 90};
+
+    writeData(&student1);
+
+    printf("Student records from file:\n");
+    readData();
 
 }
 
 void writeData(stud_t, *s)
 {
 	FILE *fw = fopen("record.txt","w");
-
+    
+    fprintf(fw, "Name: %s\nID: %d\n marks: %.2f\n", student->name, student->ID, student->marks);
+    
+    fclose(file);
 }
+
+void readData()
+{
+    FILE *file = fopen("record.txt", "r");
+    
+    stud_t student;
+
+    fscanf(file, "Name: %[^\n]\n ID: %d\n Grade: %f\n", student.name, &student.id, &student.grade);
+
+    printf("Name: %s\n ID: %d\n marks: %.2f\n", student.name, student.ID, student.marks);
+   
+    fclose(file);
+}
+
